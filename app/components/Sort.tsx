708dab7dcb4c5934b6657ec5b72d362fd36cddb8
João Fernandes <jpma.fernandes@campus.fct.unl.pt>
2026-01-22 18:26:15 +0000

    dashboard

--- app/components/Sort.tsx
+++ app/components/Sort.tsx
@@ -1,8 +1,35 @@
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
+import { sortTypes } from "@/constants";
+
 const Sort = () => {
+    const router = useRouter();
+    const path = usePathname();
+    const handleSort = (value: string) => {
+        router.push(`${path}?sort=${value}`);
+    };
+
     return (
-        <div>
-            Sort
-        </div>
+        <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+            <SelectTrigger className="sort-select">
+                <SelectValue placeholder={sortTypes[0].label} />
+            </SelectTrigger>
+            <SelectContent className="sort-select-content">
+                {sortTypes.map((sort) => (
+                    <SelectItem key={sort.label} value={sort.value} className="shad-select-item">
+                        {sort.label}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
     )
 }
 
